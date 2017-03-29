519339737a9cec4c7720b3ab8d97175fade77000
Abhishek <a2.mishra@gmail.com>
2017-03-28 21:55:54 -0700

    fined tuned is unexpectedly giving very low accuracy (same as random)

--- fine_tuned.py
+++ fine_tuned.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from keras.applications import VGG16
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, Model
@@ -11,10 +12,25 @@ batch_size = 16
 epochs = 50
 train_data_dir = "data/train/"
 validation_data_dir = "data/validation"
-weights_path = ""
+weight_path = "bottleneck_features_model.h5"
 
 #Data Generators
+train_data_gen = ImageDataGenerator(rescale=1./255, 
+                                    shear_range=30,
+                                    zoom_range=0.5,
+                                    height_shift_range=0.2,
+                                    width_shift_range=0.2)
 
+validation_data_gen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_data_gen.flow_from_directory(train_data_dir,
+                                                target_size=(150, 150),
+                                                class_mode="binary",
+                                                batch_size=16)
+validation_data = validation_data_gen.flow_from_directory(validation_data_dir,
+                                                          target_size=(150, 150),
+                                                          class_mode="binary",
+                                                          batch_size=16)
 
 #pretrained model
 input_layer = Input(shape=(150, 150, 3))
@@ -25,10 +41,21 @@ top_model.add(Flatten(input_shape=base_model.output_shape[1:]))
 top_model.add(Dense(64, activation="relu"))
 top_model.add(Dense(1, activation="sigmoid"))
 #load weights
-top_model.load_weights("bottleneck_features_model.h5")
+top_model.load_weights(weight_path)
 
 #custom model on top of VGG1
 model = Model(inputs=base_model.input, outputs=top_model(base_model.output))
+
+model.compile(optimizer="rmsprop",
+              loss="binary_crossentropy",
+              metrics=["accuracy", "binary_crossentropy"])
+
+history = model.fit_generator(
+                     train_data, 
+                     steps_per_epoch=training_samples // batch_size,
+                     epochs=epochs,
+                     validation_data=validation_data,
+                     validation_steps=validation_samples // batch_size)
 
 
 
